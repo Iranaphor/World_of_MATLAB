2f3763dfe4786f985e4b390230012e6b46c236e9
Gab2k <60928508+Gab2K@users.noreply.github.com>
2020-02-12 16:32:25 +0000

    Init

---
+++ TimeWasting.m
@@ -0,0 +1,201 @@
+close, clc, clear
+% tic
+% Setup Window Details
+global Window_Settings;
+Window_Settings.Docked = false;
+supplot(1,1,1,1);
+generateWorld()
+
+function generateWorld(varargin)
+    disp(varargin)
+    % Setup World Details
+    global World_Data;
+    World_Data.Size=1000; W=World_Data.Size;
+    World_Data.PlayerBoundary=[W,W]*0.95;
+
+    World_Data.SeaLevel=0.5;%0.4859;a
+
+    World_Data.Stars=(rand(3,0.005*(W*W)).*[2000,2000,0.2]')+[-500,-500,0.7]';
+    
+    World_Data.Map=rand(W,W);
+    World_Data.Map(45:55,45:55)=World_Data.Map(45:55,45:55)+0.2;
+    for i=1:19; World_Data.Map=smoothdata(World_Data.Map'); end
+    World_Data.Map(World_Data.Map<(0.002+World_Data.SeaLevel))=World_Data.SeaLevel;
+
+
+    % Setup Player Details
+    global Player_Data;
+    X=W/2;
+    Y=W/2;
+    Player_Data.X = X;
+    Player_Data.Y = Y;
+    Player_Data.Z = World_Data.Map(X,Y);
+    Player_Data.Angle = 0;
+    Player_Data.ScatterPlot=NaN;
+    Player_Data.OffsetZ = 0.0;
+
+    % Setup Camera Details
+    global Camera_Data;
+    Camera_Data.Render_Distance = 200; 
+    Camera_Data.CameraPosition = [-50 50 1];
+    Camera_Data.Yaw = 0;
+    Camera_Data.Pitch = 0;
+
+    renderOnce();
+end
+
+function KeyPress(~,event)
+    global Window_Settings;
+    global Player_Data;
+    global World_Data;
+    KEY = string(event.Key);
+    
+    
+    %Dock Window
+    if (KEY == "escape")
+        if (Window_Settings.Docked == true)
+            set(gcf, 'WindowStyle', 'Normal');
+        else
+            set(gcf, 'WindowStyle', 'Docked'); 
+        end
+        Window_Settings.Docked = ~Window_Settings.Docked;
+        return;
+    end
+    
+    clc
+    %Move Player
+    A=Player_Data.Angle;
+    Xn = Player_Data.X;
+    Yn = Player_Data.Y;
+    
+    if (KEY == "w")
+        Xn = Xn - sind(A);
+        Yn = Yn - cosd(A);
+    elseif (KEY == "s")
+        Xn = Xn + sind(A);
+        Yn = Yn + cosd(A);
+    elseif (KEY == "a")
+        A = A - 10;
+    elseif (KEY == "d")
+        A = A + 10;
+    end
+    
+    
+    %Boundary Management
+    S=World_Data.PlayerBoundary;
+    if Xn > S(1); Xn = S(1); end
+    if Yn > S(2); Yn = S(2); end
+    if Xn < 1; Xn = 1; end
+    if Yn < 1; Yn = 1; end
+    
+    
+    %Set specific Z value
+    X1 = floor(Xn);
+    Y1 = floor(Yn);
+    X2 = ceil(Xn)+(floor(Xn)==Xn);
+    Y2 = ceil(Yn)+(floor(Yn)==Yn);
+    R = World_Data.Map(Y1:Y2,X1:X2);
+    
+    Px=floor(10*(Xn-floor(Xn)))+1;
+    Rx1 = linspace(R(1,1),R(1,2),10);
+    Rx2 = linspace(R(2,1),R(2,2),10);
+    Y3=Rx1(Px);
+    Y4=Rx2(Px);
+    
+    Py=floor(10*(Yn-floor(Yn)))+1;
+    Ry=linspace(Y3,Y4,10);
+    Zn=Ry(Py);
+    
+    
+    %Check for Cliff Movement
+%     disp(Zn+" <= "+Player_Data.Z + " ?")
+    if (Zn<=Player_Data.Z)
+        disp("Zn>Z")
+        Player_Data.X = Xn; %Only save new positions if movement is valid.
+        Player_Data.Y = Yn;
+        Player_Data.Z=Zn;
+        Player_Data.Angle = A;
+    end
+    
+    render();
+end
+
+function renderOnce()
+    
+    global World_Data;
+    global Player_Data;
+
+    %Display World
+    World_Data.MapSurf=surf(World_Data.Map); hold on
+    ax=gca; ax.Projection = 'perspective';
+    set(ax,'Zlim',[0,2]);
+    shading interp;
+    c=colormap(summer); c(1,:)=[0.15,0.55,0.85]; colormap(c)
+    
+    World_Data.StarScatter = scatter3(World_Data.Stars(1,:),World_Data.Stars(2,:),World_Data.Stars(3,:),1,'*','w');
+    
+    Player_Data.ScatterPlot = scatter3(Player_Data.X,Player_Data.Y,Player_Data.Z,200,'filled','r');
+    
+    hold off
+    
+    set(gcf,'Color',[0.05,0.1,0.3])
+    set(ax,'visible','off')
+    
+    render()
+end
+
+function render()
+    global Player_Data;
+    
+    %Update Player Location
+    AXIS = gca;
+    ScatterPlot = AXIS.Children(1);
+    ScatterPlot.XData(1) = Player_Data.X;
+    ScatterPlot.YData(1) = Player_Data.Y;
+    ScatterPlot.ZData(1) = Player_Data.Z;
+    
+    %Update Camera Target
+    cT=0.015;
+    cD=10;
+    cP=0.005;
+    cV=80;
+    
+    AXIS.CameraTarget = [Player_Data.X,Player_Data.Y,Player_Data.Z+cT];
+    
+    %Update Camera Position
+    CamDist=cD;
+    CamX = Player_Data.X+(CamDist*sind(Player_Data.Angle));
+    CamY = Player_Data.Y+(CamDist*cosd(Player_Data.Angle));
+    CamPitch = Player_Data.Z+cP+cT;
+    AXIS.CameraPosition  = [CamX, CamY, CamPitch];
+    AXIS.CameraViewAngle = cV;
+    
+%     global Camera_Data;
+%     AXIS.XLim =[Player_Data.X-Camera_Data.Render_Distance,Player_Data.X+Camera_Data.Render_Distance];
+%     AXIS.YLim =[Player_Data.Y-Camera_Data.Render_Distance,Player_Data.Y+Camera_Data.Render_Distance];
+    
+    %disp(toc)
+    set(gcf,'KeyPressFcn',@KeyPress);
+    %tic
+    renderUI()
+end
+
+function renderUI()
+    global UI_Components;
+    
+    %Generate New World
+    UI_Components.pb = uicontrol('style','push',...
+                 'units','pix',...
+                 'position',[10 10 180 40],...
+                 'fontsize',14,...
+                 'string','Generate New World',...
+                 'callback',{@generateWorld});
+             
+             %minimap?         
+    
+end
+
+
+
+
+
---
+++ supplot.m
@@ -0,0 +1,14 @@
+function ax = supplot(W,H,X,Y,hide_bar)
+    
+    i = (X-1)/W;
+    j = (Y-1)/H;
+    
+    ax = subplot('Position',[i,j,1/W,1/H]);
+    
+    if ~(nargin == 5)
+        set(gcf,'menubar','none','NumberTitle','off');
+    else
+        set(gcf,'menubar','figure','NumberTitle','on');
+    end
+    
+end
