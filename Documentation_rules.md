6612c3b7a5a92cc61addccf4468d7e2985e2daa1
Jack Stevenson <A-Yakkus@users.noreply.github.com>
2020-02-19 16:56:59 +0000

    Set up \'official\' documentation rules
    Provide an example of how documentation should work.

---
+++ Documentation_rules.md
@@ -0,0 +1,8 @@
+# Documentation Rules
+Aim to keep one function/class per file. Whilst this does increase number of
+files to manage, it should make things simpler down the line and keep code 
+clean.
+
+Functions Docs should use standard MATLAB syntax, then include the function
+author and version/since tags and finally a list of input parameters and 
+output parameters in javadocs style.
--- Support/spawnPlane.m
+++ Support/spawnPlane.m
@@ -1,4 +1,23 @@
 function spawnPlane(c,tag,varargin)
+    %% SPAWNPLANE spawns a 2d plain in world
+    %
+    % SPAWNPLANE(c, tag, varargin) sets the colour (c) and the
+    %   identification tag of the plane. varargin is a series of
+    %   3 dimensional co-ordinates, one of which should be constant
+    %   between all points
+    %
+    %   Example
+    %     SPAWNPLANE('r', "Example", [0,0,0],[0,1,0],[1,1,0],[1,0,0])
+    %       spawns a red unit square at the origin on the XY plane.
+    %
+    %   @author Iranaphor
+    %   @version 0.0.1
+    %   @since 0.0.1
+
+    %
+    % @param c: Colour of the plane
+    % @param tag: Identification tag of the plane
+    % @param varargin: List of points
 
     %P1: [x1, y1, z1]
     X=zeros(length(varargin),1);
