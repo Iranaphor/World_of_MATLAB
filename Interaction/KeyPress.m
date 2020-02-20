af05b20f4a2a54c09d01d6573e6e95e4ca84c74c
James Heselden <34611419+Iranaphor@users.noreply.github.com>
2020-02-20 12:37:49 +0000

    Testing with new window management structures

--- Interaction/KeyPress.m
+++ Interaction/KeyPress.m
@@ -6,27 +6,14 @@ function KeyPress(~,event)
     global World_Data;
     global Visuals_Manager;
     KEY = string(event.Key);
-    disp(KEY)
-    %Dock Window
-    if (KEY == "escape")
-        if (Visuals_Manager.Docked == true)
-            set(gcf, 'WindowStyle', 'Normal');
-        else
-            set(gcf, 'WindowStyle', 'Docked'); 
-        end
-        Visuals_Manager.Docked = ~Visuals_Manager.Docked;
-        return;
+    
+    
+    %Reposition Window
+    window_modifiers=["escape","f11"];
+    if any(contains(window_modifiers,KEY))
+        Visuals_Manager.AdjustWindow(KEY);
     end
-    if (KEY=="f11")
-        disp("F11")
-        if (Visuals_Manager.Fullscreen == true)
-            set(gcf, 'Position', Visuals_Manager.OldPosition, 'Units', 'pixels')
-        else
-            Visuals_Manager.OldPosition = get(gcf,'Position');
-            set(gcf, 'Position', [0,0,1,1], 'Units', 'normalized')
-        end
-    end
-        
+
     %Move Player
     a=["w","a","s","d"];
     if any(contains(a,KEY))
